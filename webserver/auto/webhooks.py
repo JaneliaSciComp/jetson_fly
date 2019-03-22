0a2abaf36c0212f690741e3fff199f47a76d18d2
porterbot <schauderd@janelia.hhmi.org>
2019-03-22 10:47:28 -0400

    updated paths for new fictrac version

--- webserver/auto/webhooks.py
+++ webserver/auto/webhooks.py
@@ -88,6 +88,8 @@ def kill_fictrac():
 @app.route('/capture', methods = ['GET','POST'])
 def capture_photo():
     resolution = request.form.get("resolution")
+    if os.path.exists("/tmp/screengrab.jpg"):
+        os.remove("/tmp/screengrab.jpg")
     cmd = "gst-launch-1.0 nvcamerasrc num-buffers=1 flicker=0 auto-exposure=1 color-effect=1 wbmode=0 ! 'video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)I420, framerate=(fraction)60/1' ! nvjpegenc ! filesink location=/tmp/screengrab.jpg"
     snapshot = subprocess.Popen (cmd, shell=True)
     snapshot.wait()
